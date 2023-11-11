06f7b1f5ac3ec96f5d2dd69d2432515c4ca81127
Hiratake <hiratake@hiratake.xyz>
2023-11-11 11:55:55 +0900

    fix: tailwindcssが適用されていない問題を修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['@/assets/css/tailwind.css'],
   devtools: { enabled: true },
   eslint: {
     lintOnStart: false,
